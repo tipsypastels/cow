308272f970cf4857676449e2d353be73b017733a
tipsypastels <dakotasankey@gmail.com>
2025-03-09 09:55:47 -0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
+// @ts-expect-error we don't have @types/node.
+const repo: string | undefined = process.env.GITHUB_REPO;
+
 export default defineConfig({
+  base: repo ? `/${repo}/` : undefined,
   plugins: [
     tailwindcss(),
   ],
